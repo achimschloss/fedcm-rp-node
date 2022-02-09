13361b5811d32854cf06fa9c25871705bbee00ac
Glitch (glitch-hello-node) <none>
2022-02-09 07:58:55 +0000

    🍫📟 Checkpoint ./views/home.html:343052/3447 ./public/client.js:343052/685 ./views/index.html:343052/1301

--- public/client.js
+++ public/client.js
@@ -1,41 +1,15 @@
 import { html, render } from 'lit';
 
-const $ = document.querySelector.bind(document);
+export const $ = document.querySelector.bind(document);
 
-if (!'FederatedCredential' in window || !window.FederatedCredential.revoke) {
-  $('#sign-in').addEventListener('click', e => {
-    e.preventDefault();
-    signIn();
-  })
-} else {
-  $('#unsupported').classList.remove('hidden');  
-}
-
-const signIn = async () => {
-  try {
-    const result = await navigator.credentials.get({
-      federated: {
-        providers: [{
-          url: '',
-          clientId: '',
-          nonce: 1111
-        }]
-      }
-    });
-    // TODO: Verify the id token
-    // TODO: If verified, display the profile and turn the sign-in button into sign-out.
-  } catch (e) {
-    console.error(e);
-    toast(e);
-  }
-}
-
-const toast = (text) => {
+export const toast = (text) => {
   $('#snackbar').labelText = text;
   $('#snackbar').show();
 }
 
-const displayProfile = (profile) => {
+export const displayProfile = (profile) => {
   render(html`<div>
   </div>`, $('#profile'));
 }
+
+export { html, render };
