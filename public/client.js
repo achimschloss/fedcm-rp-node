0c08ec1109887d41a067967ca3d4aba7cdb7275d
Glitch (glitch-hello-node) <none>
2022-03-01 02:27:57 +0000

    🍶🦃 Checkpoint ./public/components.js:343052/63 ./public/client.js:343052/820 ./views/index.html:343052/59

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-import { html, render } from './components-bundle.js';
+import { html, render } from 'https://unpkg.com/lit@2.2.0/index.js?module';
 
 export const $ = document.querySelector.bind(document);
 
@@ -11,3 +11,27 @@ export const displayProfile = (profile) => {
   render(html`<div>
   </div>`, $('#profile'));
 }
+
+export const _fetch = async (path, payload = '') => {
+  const headers = {
+    'X-Requested-With': 'XMLHttpRequest',
+  };
+  if (payload && !(payload instanceof FormData)) {
+    headers['Content-Type'] = 'application/json';
+    payload = JSON.stringify(payload);
+  }
+  const res = await fetch(path, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: headers,
+    body: payload,
+  });
+  if (res.status === 200) {
+    // Server authentication succeeded
+    return res.json();
+  } else {
+    // Server authentication failed
+    const result = await res.json();
+    throw result.error;
+  }
+};
--- public/components.js
+++ public/components.js
@@ -1,9 +1,5 @@
-import { html, render } from 'lit';
-
 import '@material/mwc-button';
 import '@material/mwc-fab';
 import '@material/mwc-top-app-bar-fixed';
 import '@material/mwc-textfield';
 import '@material/mwc-snackbar';
-
-export { html, render };
