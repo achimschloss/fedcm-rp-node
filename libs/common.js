7fa6b13ef753efdab3891b665f3121988f220ef9
Glitch (glitch-hello-node) <none>
2023-06-02 17:11:17 +0000

    📮🎺 Checkpoint ./libs/common.js:42073211/3203

--- libs/common.js
+++ libs/common.js
@@ -1,22 +1,25 @@
-const low = require('lowdb');
-const fs = require('fs');
+const low = require("lowdb");
+const fs = require("fs");
 
-if (!fs.existsSync('./.data')) {
-  fs.mkdirSync('./.data');
+if (!fs.existsSync("./.data")) {
+  fs.mkdirSync("./.data");
 }
 
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('.data/db.json');
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync(".data/db.json");
 const db = low(adapter);
+//db.defaults({ users: [] }).write();
 
 function csrfCheck(req, res, next) {
-  if (req.header('X-Requested-With') === 'XMLHttpRequest' ||
-      req.header('Sec-FedCM-CSRF') === '?1') {
+  if (
+    req.header("X-Requested-With") === "XMLHttpRequest" ||
+    req.header("Sec-FedCM-CSRF") === "?1"
+  ) {
     next();
   } else {
-    return res.status(400).json({ error: 'Invalid access.' });
+    return res.status(400).json({ error: "Invalid access." });
   }
-};
+}
 
 /**
  * Checks CSRF protection using custom header `X-Requested-With`
@@ -24,30 +27,34 @@ function csrfCheck(req, res, next) {
  **/
 function sessionCheck(req, res, next) {
   if (!req.session.user_id) {
-    res.status(401).json({ error: 'not signed in.' });
+    res.status(401).json({ error: "not signed in." });
     return;
   }
-  const user = db.get('users').find({ user_id: req.session.user_id }).value();
+  const user = db.get("users").find({ user_id: req.session.user_id }).value();
   if (!user) {
-    return res.status(401).json({ error: 'User not found.' });
+    return res.status(401).json({ error: "User not found." });
   }
   res.locals.user = user;
 
   next();
-};
+}
 
-function getUser(user_id, username = '', name = '', picture = '') {
-    // See if account already exists
-  let user = db.get('users').find({ user_id }).value();
+function getUser(user_id, username = "", name = "", picture = "") {
+  // See if account already exists
+  let user = db.get("users").find({ user_id }).value();
   // If user entry is not created yet, create one
   if (!user) {
     user = {
       user_id,
       username,
       name,
-      picture
+      picture,
     };
-    db.get('users').push(user).write();
+    try {
+      db.get("users").push(user).write();
+    } catch (error) {
+      console.error("Error writing to the database:", error);
+    }
   }
   return user;
 }
