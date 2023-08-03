7bf8044d760788ccfa2d6e7839d8f2ec6cccf54d
Glitch (glitch-hello-node) <none>
2023-08-03 09:40:25 +0200

    FX db initialization

--- libs/common.js
+++ libs/common.js
@@ -13,31 +13,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Portions of this code are derived from the FedCM Demo 
- * https://fedcm-rp-demo.glitch.me/, licensed under the Apache License, 
+ * Portions of this code are derived from the FedCM Demo
+ * https://fedcm-rp-demo.glitch.me/, licensed under the Apache License,
  * Version 2.0 by 2019 Google Inc. Copyright 2019
-*/
+ */
 
-const low = require("lowdb");
-const fs = require("fs");
+const low = require('lowdb')
+const fs = require('fs')
 
-if (!fs.existsSync("./.data")) {
-  fs.mkdirSync("./.data");
+if (!fs.existsSync('./.data')) {
+  fs.mkdirSync('./.data')
 }
 
-const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync(".data/db.json");
-const db = low(adapter);
-//db.defaults({ users: [] }).write();
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('.data/db.json')
+const db = low(adapter)
+db.defaults({ users: [] }).write()
 
-function csrfCheck(req, res, next) {
+function csrfCheck (req, res, next) {
   if (
-    req.header("X-Requested-With") === "XMLHttpRequest" ||
-    req.header("Sec-FedCM-CSRF") === "?1"
+    req.header('X-Requested-With') === 'XMLHttpRequest' ||
+    req.header('Sec-FedCM-CSRF') === '?1'
   ) {
-    next();
+    next()
   } else {
-    return res.status(400).json({ error: "Invalid access." });
+    return res.status(400).json({ error: 'Invalid access.' })
   }
 }
 
@@ -45,35 +45,35 @@ function csrfCheck(req, res, next) {
  * Checks CSRF protection using custom header `X-Requested-With`
  * If the session doesn't contain `signed-in`, consider the user is not authenticated.
  **/
-function sessionCheck(req, res, next) {
+function sessionCheck (req, res, next) {
   if (!req.session.user_id) {
-    res.status(401).json({ error: "not signed in." });
-    return;
+    res.status(401).json({ error: 'not signed in.' })
+    return
   }
-  const user = db.get("users").find({ user_id: req.session.user_id }).value();
+  const user = db.get('users').find({ user_id: req.session.user_id }).value()
   if (!user) {
-    return res.status(401).json({ error: "User not found." });
+    return res.status(401).json({ error: 'User not found.' })
   }
-  res.locals.user = user;
+  res.locals.user = user
 
-  next();
+  next()
 }
 
-function getUser(user_id, username = "", name = "", picture = "") {
+function getUser (user_id, username = '', name = '', picture = '') {
   // See if account already exists
-  let user = db.get("users").find({ user_id }).value();
+  let user = db.get('users').find({ user_id }).value()
   // If user entry is not created yet, create one
   if (!user) {
     user = {
       user_id,
       username,
       name,
-      picture,
-    };
+      picture
+    }
     try {
-      db.get("users").push(user).write();
+      db.get('users').push(user).write()
     } catch (error) {
-      console.error("Error writing to the database:", error);
+      console.error('Error writing to the database:', error)
     }
   } else {
     // If user entry already exists, update it
@@ -82,16 +82,15 @@ function getUser(user_id, username = "", name = "", picture = "") {
       ...user,
       username,
       name,
-      picture,
-    };
+      picture
+    }
     try {
-      db.get("users").find({ user_id }).assign(user).write();
+      db.get('users').find({ user_id }).assign(user).write()
     } catch (error) {
-      console.error("Error updating the database:", error);
+      console.error('Error updating the database:', error)
     }
   }
-  return user;
+  return user
 }
 
-
-module.exports = { csrfCheck, sessionCheck, getUser };
+module.exports = { csrfCheck, sessionCheck, getUser }
